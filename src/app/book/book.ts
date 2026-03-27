1239c4d39460246659842bd77f038268ddfd5e6c
alex <a1980dominov@gmail.com>
2026-03-27 22:43:07 +0200

    my project

---
+++ src/app/book/book.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-book',
+  imports: [],
+  templateUrl: './book.html',
+  styleUrl: './book.css',
+})
+export class Book {
+  
+   
+}
